3d1a5cd4a93be81bb455b5b12e98e81ec8fae286
kkeyy <kkeyy-hash@protonmail.com>
2023-03-21 21:00:50 +0100

    Update MenuManager.lua

--- addons/MenuManager.lua
+++ addons/MenuManager.lua
@@ -1,19 +1,16 @@
 local MenuManager = {} do
     MenuManager.Library = nil
     
-    function MenuManager:SetLibrary(lib)
-        self.Library = lib
-    end
-    
-    function MenuManager:BuildMenuSection(tab)
-	MenuManager.Library:OnUnload(function()
-            MenuManager.Library.Unloaded = true
+    function MenuManager:BuildMenuSection(tab, lib)
+	if self.Library == nil then self.Library = lib end
+	self.Library:OnUnload(function()
+            self.Library.Unloaded = true
         end)
 
         local MenuGroup = tab:AddLeftGroupbox("Menu")
         MenuGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", { Default = "LeftAlt", NoUI = true, Text = "Menu keybind" })
         MenuGroup:AddButton("Unload", function() 
-            MenuManager.Library:Unload() 
+            self.Library:Unload() 
         end)
       	MenuGroup:AddButton("Copy Discord", function()
       	    setclipboard("https://discord.gg/8dxUzPna86")
@@ -35,7 +32,7 @@ local MenuManager = {} do
         	    })
    	        })
        	end)
-        MenuManager.Library.ToggleKeybind = Options.MenuKeybind
+        self.Library.ToggleKeybind = Options.MenuKeybind
     end
 end
 return MenuManager
