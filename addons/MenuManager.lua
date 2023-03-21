67648577b486fb44f883d10b7839dd56658c6e06
kkeyy <kkeyy-hash@protonmail.com>
2023-03-21 20:49:31 +0100

    Create MenuManager.lua

---
+++ addons/MenuManager.lua
@@ -0,0 +1,31 @@
+local MenuManager = {} do
+	function MenuManager:BuildConfigSection(tab)
+		local MenuGroup = Script.Tabs["Settings"]:AddLeftGroupbox("Menu")
+    MenuGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", { Default = "LeftAlt", NoUI = true, Text = "Menu keybind" })
+    MenuGroup:AddButton("Unload", function() 
+        Library:Unload() 
+    end)
+    MenuGroup:AddButton("Copy Discord", function()
+        setclipboard("https://discord.gg/8dxUzPna86")
+    end)
+    MenuGroup:AddButton("Join Discord", function()
+        syn.request({
+            Url = "http://127.0.0.1:6463/rpc?v=1",
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json",
+                ["origin"] = "https://discord.com",
+            },
+            Body = game:GetService("HttpService"):JSONEncode({
+                ["args"] = {
+                    ["code"] = "8dxUzPna86",
+                },
+                ["cmd"] = "INVITE_BROWSER",
+                ["nonce"] = "."
+            })
+        })
+    end)
+    Library.ToggleKeybind = Options.MenuKeybind
+	end
+end
+return MenuManager
